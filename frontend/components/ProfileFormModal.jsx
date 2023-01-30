7645abceebfa5cb8b2c519ff2c47d4cbfb9886ec
Steven <codebusiness2014@gmail.com>
2023-01-30 17:04:15 -0600

    Update modal for making updates to user profile

--- frontend/components/ProfileFormModal.jsx
+++ frontend/components/ProfileFormModal.jsx
@@ -15,19 +15,37 @@ const ProfileFormModal = () => {
     setUserData({ ...userData, [e.target.name]: e.target.value });
   };
 
-  const onSubmit = () => {};
+  const onSubmitUpdate = () => {};
+
+  const onCloseModal = () => {
+    setShowModal((prev) => !prev);
+  };
+
+  const onCancelUpdate = () => {
+    onCloseModal();
+  };
 
   return (
     <>
-      <button
-        data-modal-target="defaultModal"
-        data-modal-toggle="defaultModal"
-        class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
+      <div
+        className="ml-2 cursor-pointer"
         onClick={() => setShowModal((prev) => !prev)}
       >
-        Toggle modal
-      </button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+          />
+        </svg>
+      </div>
 
       {showModal && (
         <div
@@ -37,7 +55,7 @@ const ProfileFormModal = () => {
           class="absolute inset-0 h-100"
         >
           <div class="relative w-full h-full md:h-auto">
-            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 p-14">
               <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                 <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
                   Update Profile
@@ -46,7 +64,7 @@ const ProfileFormModal = () => {
                   type="button"
                   class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
                   data-modal-hide="defaultModal"
-                  onClick={() => setShowModal((prev) => !prev)}
+                  onClick={onCloseModal}
                 >
                   <svg
                     aria-hidden="true"
@@ -64,7 +82,7 @@ const ProfileFormModal = () => {
                   <span class="sr-only">Close modal</span>
                 </button>
               </div>
-              <form action="#">
+              <form action="#" className="mt-8">
                 <div className="grid gap-4 mb-4">
                   <div className="grid sm:grid-cols-2 gap-5">
                     <div>
@@ -106,6 +124,38 @@ const ProfileFormModal = () => {
                   </div>
                   <div>
                     <label
+                      htmlFor="aboutme"
+                      className="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white"
+                    >
+                      About me
+                    </label>
+                    <textarea
+                      id="aboutme"
+                      rows="4"
+                      class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Write a small description about yourself..."
+                    ></textarea>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="title"
+                      className="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white"
+                    >
+                      Company
+                    </label>
+                    <input
+                      type="text"
+                      name="title"
+                      id="title"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-lg rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Software Engineer - Not required"
+                      value={userData.title}
+                      onChange={onInputChange}
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
                       htmlFor="title"
                       className="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white"
                     >
@@ -124,99 +174,70 @@ const ProfileFormModal = () => {
                   </div>
                   <div>
                     <label
-                      htmlFor="email"
+                      htmlFor="title"
                       className="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white"
                     >
-                      Email
+                      Years of experience
                     </label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-lg rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="email@gmail.com"
-                      value={userData.email}
-                      onChange={onInputChange}
-                      required="*"
-                    />
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="password"
-                      className="block mb-2 text-sm md:text-lg font-medium text-gray-900 dark:text-white"
+                    <select
+                      id="countries"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm md:text-lg rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="•••••••••"
-                      value={userData.password}
-                      onChange={onInputChange}
-                      required=""
-                    />
-                  </div>{" "}
-                  <div>
-                    <label
-                      htmlFor="confirmPassword"
-                      className="block mb-2 text-sm md:text-lg font-medium text-gray-900 dark:text-white"
-                    >
-                      Confirm password
-                    </label>
-                    <input
-                      type="password"
-                      name="confirmPassword"
-                      id="confirmPassword"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm md:text-lg rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="•••••••••"
-                      value={userData.confirmPassword}
-                      onChange={onInputChange}
-                      required=""
-                    />
+                      <option selected>Choose experience range</option>
+                      <option value="j">Junior (0 - 3)</option>
+                      <option value="m">Mid (3 - 6)</option>
+                      <option value="s">Senior (6+)</option>
+                    </select>
                   </div>
+                  <label class="relative text-zinc-200 inline-flex items-center cursor-pointer">
+                    <input type="checkbox" value="" class="sr-only peer" />
+                    <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                      Open for work
+                    </span>
+                  </label>
+                  <label class="relative text-zinc-200 inline-flex items-center cursor-pointer">
+                    <input type="checkbox" value="" class="sr-only peer" />
+                    <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                      Recently laidoff (under a year)
+                    </span>
+                  </label>
                 </div>
-                <button
-                  onClick={onSubmit}
-                  type="submit"
-                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-blue-700  py-2 px-4 text-sm md:text-lg font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-                      />
-                    </svg>
-                  </span>
-                  Register
-                </button>
+                <div class="">
+                  <label
+                    htmlFor="resume"
+                    className="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white"
+                  >
+                    Resume
+                  </label>
+                  <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                    <input
+                      class="block w-full text-sm text-gray-900 border border-gray-300 cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                      id="file_input"
+                      type="file"
+                    />
+                  </dd>
+                </div>
+                <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                  <button
+                    data-modal-hide="defaultModal"
+                    type="button"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    onClick={onSubmitUpdate}
+                  >
+                    Update
+                  </button>
+                  <button
+                    data-modal-hide="defaultModal"
+                    type="button"
+                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                    onClick={onCancelUpdate}
+                  >
+                    Cancel
+                  </button>
+                </div>
               </form>
-              <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <button
-                  data-modal-hide="defaultModal"
-                  type="button"
-                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Update
-                </button>
-                <button
-                  data-modal-hide="defaultModal"
-                  type="button"
-                  class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-                >
-                  Cancel
-                </button>
-              </div>
             </div>
           </div>
         </div>
--- frontend/pages/dashboard/[id].js
+++ frontend/pages/dashboard/[id].js
@@ -58,26 +58,6 @@ const Dashboard = () => {
             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">No</dd>
           </div>
           <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt class="text-sm font-medium text-gray-500">Open for work</dt>
-            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              <label class="relative inline-flex items-center cursor-pointer">
-                <input type="checkbox" value="" class="sr-only peer" />
-                <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-              </label>
-            </dd>
-          </div>{" "}
-          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt class="text-sm font-medium text-gray-500">
-              Laid off (under a year)
-            </dt>
-            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              <label class="relative inline-flex items-center cursor-pointer">
-                <input type="checkbox" value="" class="sr-only peer" />
-                <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-              </label>
-            </dd>
-          </div>
-          <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt class="text-sm font-medium text-gray-500">Profile Image</dt>
             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
               <div class="flex flex-wrap justify-center">
