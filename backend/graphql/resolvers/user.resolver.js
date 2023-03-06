21c0592bbb275f22f9f5cb14d0906312108c6e74
Steven <codebusiness2014@gmail.com>
2023-03-05 22:18:06 -0600

    Update auth sql from orm to raw

--- backend/graphql/resolvers/user.resolver.js
+++ backend/graphql/resolvers/user.resolver.js
@@ -1,71 +1,3 @@
-// {
-//     id: 1,
-//     firstName: "Bonnie",
-//     lastName: "Green",
-//     initials: "BG",
-//     imageUrl:
-//       "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-//     title: "Visual Designer",
-//     company: "Google",
-//     openForWork: false,
-//     socials: [
-//       { facebook: "" },
-//       { linkedIn: "" },
-//       { gitHub: "" },
-//       { website: "" },
-//     ],
-//   },
-//   {
-//     id: 2,
-//     firstName: "Steve",
-//     lastName: "Oss",
-//     initials: "SO",
-//     imageUrl:
-//       "https://as2.ftcdn.net/v2/jpg/02/24/86/95/1000_F_224869519_aRaeLneqALfPNBzg0xxMZXghtvBXkfIA.jpg",
-//     title: "Frontend Developer",
-//     company: "",
-//     openForWork: false,
-//     socials: [
-//       { facebook: "" },
-//       { linkedIn: "" },
-//       { gitHub: "" },
-//       { website: "" },
-//     ],
-//   },
-//   {
-//     id: 3,
-//     firstName: "Mike",
-//     lastName: "Miller",
-//     initials: "MM",
-//     imageUrl:
-//       "https://as2.ftcdn.net/v2/jpg/02/24/86/95/1000_F_224869519_aRaeLneqALfPNBzg0xxMZXghtvBXkfIA.jpg",
-//     title: "Frontend Developer",
-//     company: "",
-//     openForWork: false,
-//     socials: [
-//       { facebook: "" },
-//       { linkedIn: "" },
-//       { gitHub: "" },
-//       { website: "" },
-//     ],
-//   },
-//   {
-//     id: 4,
-//     firstName: "Jen",
-//     lastName: "Lu",
-//     initials: "JL",
-//     imageUrl: "",
-//     title: "Frontend Developer",
-//     company: "",
-//     openForWork: false,
-//     socials: [
-//       { facebook: "" },
-//       { linkedIn: "" },
-//       { gitHub: "" },
-//       { website: "" },
-//     ],
-//   },
-
 const jwt = require("jsonwebtoken");
 const { hash, compare } = require("bcrypt");
 const { GraphQLError } = require("graphql");
@@ -88,97 +20,99 @@ const checkPassword = (password, realPassword) =>
 
 const signup = async (_, args, contextValue) => {
   const { email, password, firstName, lastName, title } = args;
-  const { db } = contextValue;
+  const { client } = contextValue;
   const hashedPassword = await hash(password, Number(process.env.SALT_ROUNDS));
-  const user = await db
-    .transaction(function (trx) {
-      trx
-        .insert({ email: email, password: hashedPassword })
-        .into("users")
-        .returning("id")
-        .then((res) => {
-          const { id } = res[0];
-          console.log("id is ", id);
-          return trx
-            .insert({
-              first_name: firstName,
-              last_name: lastName,
-              initials: firstName[0] + lastName[0],
-              title: title,
-              user_id: id,
-            })
-            .into("profiles")
-            .then(() => {
-              return trx("users")
-                .where("users.id", id)
-                .select("*")
-                .returning(["id, email"]);
-            });
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
-    })
-    .then(function (resp) {
-      return resp[0];
-    })
-    .catch((err) => {
-      console.log("err is ", err);
-      throw new GraphQLError({
-        extensions: {
-          code: "INTERNAL_ERROR",
-        },
-      });
+  try {
+    await client.query("BEGIN");
+    // Insert user data
+    const userQuery =
+      "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id, email";
+    const userData = [email, hashedPassword];
+    const userResult = await client.query(userQuery, userData);
+
+    const userId = userResult.rows[0].id;
+
+    // Insert profile data
+    const profileQuery =
+      "INSERT INTO profiles (user_id, first_name, last_name, initials, title) VALUES ($1, $2, $3, $4, $5)";
+    const profileData = [
+      userId,
+      firstName,
+      lastName,
+      firstName[0] + lastName[0],
+      title,
+    ];
+
+    await client.query(profileQuery, profileData);
+    await client.query("COMMIT");
+    console.log("Transaction completed successfully!");
+
+    const user = userResult.rows[0];
+    const token = signToken(user);
+
+    return {
+      token,
+      user,
+    };
+  } catch (error) {
+    await client.query("ROLLBACK");
+    console.error("Transaction failed:", error);
+    throw new GraphQLError({
+      extensions: {
+        code: "INTERNAL_ERROR",
+      },
     });
-
-  const token = signToken(user);
-
-  return {
-    token,
-    user,
-  };
+  }
 };
 
 const signin = async (_, args, contextValue) => {
   const { email, password } = args;
-  const { db } = contextValue;
+  const { db, client } = contextValue;
 
-  const user = await db
-    .select("id", "email", "password")
-    .from("users")
-    .where("email", email)
-    .returning(["id", "email", "password"])
-    .then((res) => res[0])
-    .catch(() => {
-      throw new GraphQLError({
+  try {
+    const userResult = await client.query(
+      `
+      SELECT id, email, password
+      FROM users
+      WHERE email=$1
+    `,
+      [email]
+    );
+
+    if (!userResult.rows) {
+      throw new GraphQLError("Incorrect email and password", {
         extensions: {
-          code: "INTERNAL_ERROR",
+          code: "NOT_FOUND",
         },
       });
-    });
+    }
 
-  if (!user) {
-    throw new GraphQLError("Incorrect email and password", {
+    const user = userResult.rows[0];
+
+    const isValidPassword = await checkPassword(password, user.password);
+    if (!isValidPassword) {
+      throw new GraphQLError("Incorrect email and password", {
+        extensions: {
+          code: "NOT_FOUND",
+        },
+      });
+    }
+
+    const token = signToken(user);
+
+    return {
+      token,
+      user,
+    };
+  } catch (error) {
+    await client.query("ROLLBACK");
+    console.error("Transaction failed:", error);
+    throw new GraphQLError({
       extensions: {
-        code: "NOT_FOUND",
+        code: "INTERNAL_ERROR",
       },
     });
   }
-
-  const isValidPassword = await checkPassword(password, user.password);
-  if (!isValidPassword) {
-    throw new GraphQLError("Incorrect email and password", {
-      extensions: {
-        code: "NOT_FOUND",
-      },
-    });
-  }
-
-  const token = signToken(user);
-
-  return {
-    token,
-    user,
-  };
 };
 
 module.exports = {
